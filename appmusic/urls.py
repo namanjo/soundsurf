a58b177e062482df34a5e8dcef9397279af8d5a9
namanjo <naman14joshi@gmail.com>
2020-07-14 23:41:54 +0530

    Revamped the complete User Interface
    Given a whole new look to the application & made it more appealing and user friendly. Revamped all the templates.

--- appmusic/urls.py
+++ appmusic/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('soundsurf/all/<str:its_type>/', views.all_albums_songs, name='all_albums_songs'),
     path('soundsurf/<int:album_id>/', views.detail, name='album_detail'),
     path('album/create/', views.album_create, name='album_create'),
     path('album/<int:album_id>/add-song/', views.add_song, name='add_song'),
--- appmusic/views.py
+++ appmusic/views.py
@@ -1,18 +1,52 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 from appmusic.models import Album, Songs
 from appmusic.forms import AlbumForm, AddSongForm
+from django.db.models import Q
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
-    album_obj = Album.objects.order_by('album_name')
-    return render(request, 'appmusic/index.html', {'albums': album_obj})
+    is_search=False
+    query = request.GET.get('q')
+    if query:
+        is_search = True
+        albums = Album.objects.filter(
+            Q(album_name__icontains=query) |
+            Q(album_artist__icontains=query) |
+            Q(genre__icontains=query)
+        ).distinct()
+
+        songs = Songs.objects.filter(
+        Q(song_title__icontains=query)
+        ).distinct()
+
+        context = {'albums': albums, 'songs_list': songs, 'is_search': is_search}
+        return render(request, 'appmusic/index.html', context)
+
+    albums = Album.objects.order_by('album_name')[:8]
+    songs = Songs.objects.all()[:10]
+    context = {'albums': albums, 'songs_list': songs, 'is_search':is_search}
+    return render(request, 'appmusic/index.html', context)
 
 
 def about(request):
     return render(request, 'appmusic/about.html')
+
+
+def all_albums_songs(request, its_type):
+    if its_type == "songs":
+        songs = Songs.objects.order_by('song_title')
+        context = {'songs_list': songs, 'its_type':its_type}
+        return render(request, 'appmusic/all_items.html', context)
+    elif its_type == "albums":
+        albums = Album.objects.order_by('album_name')
+        context = {'albums': albums, 'its_type':its_type}
+        return render(request, 'appmusic/all_items.html', context)
+    else:
+        raise Http404
 
 
 def detail(request, album_id):
@@ -36,11 +70,10 @@ def album_create(request):
     return render(request, 'appmusic/create_album.html', {'form': form})
 
 
-FILE_TYPE = ['mp3', 'wav']
-
 @login_required
 def add_song(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
+    FILE_TYPE = ['mp3', 'wav']
 
     if request.method == "POST":
         form = AddSongForm(request.POST, request.FILES)
@@ -64,38 +97,42 @@ def add_song(request, album_id):
 
 @login_required
 def album_delete(request, album_id):
-    album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
     album.delete()
     return redirect('index')
 
 
 @login_required
 def song_delete(request, song_id):
-    song = Songs.objects.get(pk = song_id)
+    song = get_object_or_404(Songs, pk=song_id)
     album_id = song.album.id
     song.delete()
     return redirect('album_detail', album_id=album_id)
 
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username = username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                if 'next' in request.POST:
-                    return redirect(request.POST.get('next'))
-                else:
-                    return redirect('index')
-            else:
-                return render(request, 'appmusic/login.html', {'error_message':'Account has been temporarily disabled.'})
-        else:
-            return render(request, 'appmusic/login.html', {'error_message': 'Invalid User Login'})
+    if request.user.is_authenticated:
+        return redirect('index')
+        
     else:
-        return render(request, 'appmusic/login.html')
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(username = username, password=password)
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    if 'next' in request.POST:
+                        return redirect(request.POST.get('next'))
+                    else:
+                        return redirect('index')
+                else:
+                    return render(request, 'appmusic/login.html', {'error_message':'Account has been temporarily disabled.'})
+            else:
+                return render(request, 'appmusic/login.html', {'error_message': 'Invalid User Login'})
+        else:
+            return render(request, 'appmusic/login.html')
 
 
 @login_required
