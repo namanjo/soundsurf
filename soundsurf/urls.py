3cf471c7d31473973b1c23693c300b6df0792b27
namanjo <naman14joshi@gmail.com>
2020-09-03 00:18:20 +0530

    Polished the website a bit
    Added css autoprefixes to css.

--- soundsurf/urls.py
+++ soundsurf/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('appmusic.urls')),
-    path('admin/', admin.site.urls),
+    path('administhebest/', admin.site.urls),
 ]
 
 if settings.DEBUG:
